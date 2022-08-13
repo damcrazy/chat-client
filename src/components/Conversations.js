ea760895d4d37d773d6a99bf05d7a3c69459ecf1
Kalyan <kalyanb2000@gmail.com>
2022-08-13 23:55:04 +0530

    fixed-message send

--- src/components/Conversations.js
+++ src/components/Conversations.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import { ListGroup } from 'react-bootstrap'
+import CloseButton from 'react-bootstrap/CloseButton';
 import { useConversations } from '../contexts/ConversationsProvider'
 
-
-
+import './Styles/Conversations/conversations.css'
+import CloseIcon from '@material-ui/icons/Close';
 export default function Conversations() {
-  const { Conversations,selectedConversationIndex} = useConversations();
-  // console.log(Conversations);
+  const { Conversations,selectedConversationIndex,setDel} = useConversations();
+  
+  
   return (
-    <ListGroup variant="flush">
+    <ListGroup variant="flush" className='group'>
       {Conversations.map((Conversation,index) => (
+        <div key={index}>
         <ListGroup.Item 
           key={Conversation.id}
           action
@@ -17,10 +20,17 @@ export default function Conversations() {
             selectedConversationIndex(index)}}
 
           active={Conversation.selected}
+          className="conversation-item"
         >
-          {Conversation.recipients.map(recipient => recipient.name).join(', ')}
+          {Conversation.recipients.map(recipient => recipient.name).join(', ')}      
+          
+            
         </ListGroup.Item>
+        <button onClick={() => {setDel(index)}} className="close">
+          <CloseIcon className="conversation-item__close" /></button>
+        </div>
       ))}
+
     </ListGroup>
   )
 }
